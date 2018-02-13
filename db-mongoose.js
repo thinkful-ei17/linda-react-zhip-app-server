b97be4cbe3eabfb65a32f4c96ff33da7450ac496
Linda Foo <linda.foo.dev@gmail.com>
2018-02-13 07:25:05 -0800

    first commit with starting files

---
+++ db-mongoose.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {DATABASE_URL} = require('./config');
+
+function dbConnect(url = DATABASE_URL) {
+    return mongoose.connect(url, {useMongoClient: true}).catch(err => {
+        console.error('Mongoose failed to connect');
+        console.error(err);
+    });
+}
+
+function dbDisconnect() {
+    return mongoose.disconnect();
+}
+
+function dbGet() {
+    return mongoose;
+}
+
+module.exports = {
+    dbConnect,
+    dbDisconnect,
+    dbGet
+};

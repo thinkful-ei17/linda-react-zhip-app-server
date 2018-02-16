cc9b8ec96faf7ef2744a4e256e7382ca86d943cd
Linda Foo <linda.foo.dev@gmail.com>
2018-02-15 16:08:35 -0800

    housekeeping - spaceing and bracket/parands alignment

--- index.js
+++ index.js
@@ -25,78 +25,80 @@ app.use(
     })
 );
 
-//call put in post METHODS- fetch request (node js fetch module);
-
-//POST
-
-
+//works
+//see all users for testing - server side only - run through postman or localhost: 8080
 app.get('/users', (req, res) => {
     User.find({})
        .then(user => {
-        res.json(user);
+            res.json(user);
        })
        .catch(err =>{
-         console.error(err);
-         res.status(500).json({message: 'Internal Server Error'});
+           console.error(err);
+           res.status(500).json({message: 'Internal Server Error'});
        }); //error handler
 });
-   
-//see all transactions info (ledger) - public key, transaction amt, transaction id, transaction type
+ 
+//works
+//see all transactions for testing - server side only - run through postman or localhost: 8080
 app.get('/transactions', (req, res) => {
     Transaction.find({})
         .then(transaction => {
-          res.json(transaction);
+            res.json(transaction);
         })
         .catch(err =>{
-          console.error(err);
-          res.status(500).json({message: 'Internal Server Error'});
+            console.error(err);
+            res.status(500).json({message: 'Internal Server Error'});
         }); //error handler
 });
 
+//works on server-side; need to add on client for extension of MVP
 //get one users balance
 app.get('/user/balance/:id', (req, res) => {
     const userId = req.params.id;
     User.findById(userId)
        .then(user => {
-        res.json(user);
+            res.json(user);
        })
        .catch(err =>{
-         console.error(err);
-         res.status(500).json({message: 'Internal Server Error'});
+           console.error(err);
+           res.status(500).json({message: 'Internal Server Error'});
        }); //error handler
 });
 
+//works on server-side; need to add on client for extension of MVP
 //see all transactions info for user
 app.get('/activity/transactions/:id', (req, res) => {
     const userId = req.params.id;
     Transaction.find({ $or: [{userIdInitiator: userId}, {userIdClaimer: userId}]})
         .then(transactionInitiate => {
-          res.json(transactionInitiate);
+            if (transactionInitiate) {
+                res.json(transactionInitiate);
+            }
+            else {
+                res.status(404).end(); //404 handler
+            }
         })
         .catch(err =>{
-          console.error(err);
-          res.status(500).json({message: 'Internal Server Error'});
+            console.error(err);
+            res.status(500).json({message: 'Internal Server Error'}); // error handler
         }); //error handler
 });
    
-
-//new user provides id and starting accountblance of 0
+//works on server-side; need to add on client for extension of MVP
+//new user provides id and starting accountbalance of 1000 for demo purposes
 app.post('/user/new', (req, res) => {
-    /***** Never trust users - validate input *****/
-  
-    User.create({accountBalance: 1000})
-      .then(user => {
-        if (user) {
-          res.json(user);
-        }
-      })
-      .catch(err => console.error(`Error: ${err.message}`));
-  });
 
-  //5a844476734d1d1523dba1c5
-  //5a844483734d1d1523dba1d6
+    User.create({accountBalance: 1000})
+        .then(user => {
+            if (user) {
+            res.json(user);
+        }
+        })
+        .catch(err => console.error(`Error: ${err.message}`)); // error handler
+});
+
+//works on server and client
 //sending user inputs userIdInitiator and transactionAmount => capture via req.body
-//{"transactionAmount":10,"userIdInitiator":"5a844483734d1d1523dba1d6"}
 app.post('/transaction/send', jsonParser, (req, res) => {
 
     /***** Never trust users - validate input *****/
@@ -119,18 +121,19 @@ app.post('/transaction/send', jsonParser, (req, res) => {
                         res.json(sendAmount);
                         }
                     })
+                }   
+            else {
+            res.status(404).end(); // 404 handler
             }
-        else {
-        res.status(404).end();
-        }
-    })
+        })
     .catch(err => {
         res.status(500).send({message: 'Internal Server Error'});
-    });
+    }); // error handler
 });
 
-  //{"transactionAmount":10,"userIdInitiator":"5a844483734d1d1523dba1d6"}
-  app.put('/account/send', jsonParser, (req, res) => {
+//works on server and client
+//updates sending users account to reflect deduction based on IOU amount
+app.put('/account/send', jsonParser, (req, res) => {
     const id = req.body.userIdInitiator;
     const amount = req.body.transactionAmount;
 
@@ -139,50 +142,37 @@ app.post('/transaction/send', jsonParser, (req, res) => {
   
     const missingFields = requiredFields.filter(field => !(field in req.body));
   
-   User.findById(id)
-    .then(account => {
-        if (parseInt(account.accountBalance, 10) >= parseInt(amount, 10)) { 
-        let newBalance = parseInt(account.accountBalance, 10) - parseInt(amount, 10);
-          User.findByIdAndUpdate(id, {accountBalance: newBalance})
-          .then(() => res.status(204).end())
-          .catch(err => console.error(`Error: ${err.message}`));
-        }
-        else {
-            res.status(404).end(); // 404 handler
-        }
-    })
+    User.findById(id)
+        .then(account => {
+            if (parseInt(account.accountBalance, 10) >= parseInt(amount, 10)) { 
+            let newBalance = parseInt(account.accountBalance, 10) - parseInt(amount, 10);
+                User.findByIdAndUpdate(id, {accountBalance: newBalance})
+                    .then(() => res.status(204).end())
+                    .catch(err => console.error(`Error: ${err.message}`));
+            }
+            else {
+                res.status(404).end(); // 404 handler
+            }
+        })
     .catch(err => {
-      res.status(500).send({message: 'Internal Server Error'});
+        res.status(500).send({message: 'Internal Server Error'});
     });  // error handler
-  });
+});
 
-//localhost:8080/receive/transaction/5a84a88d03eb191658d565cf
-//{"userIdClaimer": "5a844476734d1d1523dba1c5"}
+//works on server and client
+//updates transaction to reflect claim to IOU by claiming user
 app.put('/transaction/receive/:transactionId', jsonParser , (req, res) => {
-    console.log('hello I am working hard!!!!');
     const transId = req.params.transactionId;
     const id = req.body.userIdClaimer;
-
-    console.log('what is transId', transId);
-    console.log('what is userIdClaimer', id);
 
     const requiredFields = ['userIdClaimer'];
   
     const missingFields = requiredFields.filter(field => !(field in req.body));
-
-    console.log('what is tranid here', transId);
-    console.log('what is user id', id);
         Transaction.findById(transId)
             .then(transaction => {
-                console.log('what is transaction', transaction);
-                console.log('what is isIOUClaimed', transaction.isIOUClaimed);
-                console.log('what is id here in findbyId!!!!!!', id);
-                console.log('what is userIdInitiator', transaction.userIdInitiator);
                 if (transaction && !(transaction.isIOUClaimed) && (transaction.userIdInitiator !== id)) {
-                    console.log('transaction exists! now what???');
                 Transaction.findByIdAndUpdate(transId, {userIdClaimer: id, isIOUClaimed: true}, {new: true})
                     .then(completedTransaction => {
-                        console.log('what is completedTransaction');
                     if (completedTransaction) {
                         res.json(completedTransaction);
                     }
@@ -193,12 +183,13 @@ app.put('/transaction/receive/:transactionId', jsonParser , (req, res) => {
                 }
         })
         .catch(err => {
-            res.status(500).send({message: 'Internal Server Error'});
+            res.status(500).send({message: 'Internal Server Error'}); // error handler
         });
 }); 
 
 
-  //{"transactionType":"send","transactionAmount":20,"userId":"5a844483734d1d1523dba1d6"}
+//works on server and client
+//updates claiming user account to based on addition of IOU credit
 app.put('/account/receive/:transactionId', (req, res) => {
     const id = req.body.userIdClaimer;
     //have to find const amount = req.body.transactionAmount;

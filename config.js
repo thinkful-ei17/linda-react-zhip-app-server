e30c5d314877b5b8fcd161a65005b6c15be91e13
Linda Foo <linda.foo.dev@gmail.com>
2018-02-28 16:44:50 -0800

    edit env

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'heroku config:set CLIENT_ORIGIN=https://zhip.netlify.com',
     DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://guest:guest123@ds235418.mlab.com:35418/zhip',
+        process.env.DATABASE_URL || 'heroku config:set DATABASE_URL=mongodb://guest:guest123@ds235418.mlab.com:35418/zhip',
     TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/backend-test'
 };
+
